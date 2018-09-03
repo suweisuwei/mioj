284b4c101579dae4954cd860d74be2c236471f59
suweisuwei <1093775897@qq.com>
2018-09-03 23:15:39 +0800

    commit problem2

--- problem1.py
+++ problem1.py
@@ -1,12 +1,27 @@
+#coding:utf-8
 # 此处可 import 模块
 
 """
 @param string line 为单行测试数据
 @return string 处理后的结果
 """
+
+
 def solution(line):
     # 缩进请使用 4 个空格，遵循 PEP8 规范
-    a, b = line.strip().split()
+    num = int(line)
+
     # 返回处理后的结果
-    return str(int(a) + int(b))
+    return get_count(num)
+
+def get_count(num):
+    if num == 1 :
+        return 1
+    elif num == 2 :
+        return 2
+    else :
+        return get_count(num - 1) + get_count(num - 2)
+
+result = solution(5)
+print(result)
 
